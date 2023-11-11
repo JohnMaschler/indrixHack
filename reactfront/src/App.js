cbd0d9a39ccfcce5e8f2aefb778059be57ce5495
Arya P <aryap@Aryas-MacBook-Pro-2.local>
2023-11-11 15:25:04 -0800

    Initial commit

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,37 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
+import './App.css'; // Import the CSS file
+
+// ... (your imports)
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className="App">
+        <div className="menu">
+          <Link to="/" className="menu-item">Home</Link>
+          <Link to="/register" className="menu-item">Register</Link>
+          <Link to="/login" className="menu-item">Login</Link>
+        </div>
 
-        <hr />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
 }
+
 
 function Home() {
   return <h2>Home</h2>;
