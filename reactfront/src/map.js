2b978dea60d8889caefb5a6d5e7d3a3c0734ccd9
Vignesh Senthilkumar <97263689+vsenthilkumar18@users.noreply.github.com>
2023-11-11 15:07:30 -0800

    Add files via upload
    new files

---
+++ reactfront/src/map.js
@@ -0,0 +1,19 @@
+// map.js
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Map = () => (
+  <LoadScript
+    googleMapsApiKey="AIzaSyClytxU9F6_kNdH-qomR2RUWYSH5_rXHJM"
+  >
+    <GoogleMap
+      mapContainerStyle={{ width: '100%', height: '400px' }}
+      center={{ lat: 34.106478001319, lng: -117.59248180374368 }}
+      zoom={8}
+    >
+      <Marker position={{ lat: 34.106478001319, lng: -117.59248180374368 }} />
+    </GoogleMap>
+  </LoadScript>
+);
+
+export default Map;
