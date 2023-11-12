4ff157e966d67d213e9b574c7aedff833c1c74c8
Johnathon <jjmaschler@gmail.com>
2023-11-11 21:25:19 -0800

    here arya

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,44 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { auth } from './firebase';
 import Signin from './components/Signin';
-import Map from './map'; // Adjust the import
+import Map from './map';
 import Home from './components/Home';
 import Incident from './components/Incident';
-import './App.css'; // Adjust the path to match the location of your CSS file
-
-
+import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Set up a Firebase authentication observer
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      setUser(authUser);
+    });
+  
+    // Cleanup the observer when the component unmounts
+    return () => unsubscribe();
+  }, []);
+  
+  const handleSignOut = () => {
+    auth.signOut();
+  };
+  
+
   return (
     <Router>
       <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/map">Map</Link>
-          </li>
-          <li>
-            <Link to="/Sign-in">Register</Link>
-          </li>
-          <li>
-            <Link to="/incident">incidents</Link>
-          </li>
-        </ul>
-      </nav>
-
+        <nav>
+          <div className="nav-container">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/map">Map</Link>
+            </li>
+            <li>
+              {user ? (
+                <>
+                  <span>Welcome, {user.displayName}!</span>
+                  <button onClick={handleSignOut}>Sign Out</button>
+                </>
+              ) : (
+                <Link to="/signin">Sign-in</Link>
+              )}
+            </li>
+            <li>
+              <Link to="/incident">incidents</Link>
+            </li>
+          </ul>
+          </div>
+        </nav>
 
         <hr />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/map" element={<Map />} />
-          <Route path="/register" element={<Signin />} />
-          <Route path="/incident" element={<Incident />}/>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/incident" element={<Incident />} />
         </Routes>
-
       </div>
     </Router>
   );
